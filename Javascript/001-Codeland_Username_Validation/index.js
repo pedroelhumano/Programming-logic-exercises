8c3dbc70e7fcc2f5a90af9cf3bde699e7e888bcd
Pedro Yanez <pedro_y91@outlook.com>
2022-06-18 19:48:21 -0400

    rename folder

---
+++ Javascript/001-Codeland_Username_Validation/index.js
@@ -0,0 +1,11 @@
+function CodelandUsernameValidation(str) {
+  // code goes here
+  if (str.length > 3 && str.length < 25) {
+    const validUser = /^[A-Za-z]+[A-Za-z0-9_]+[A-Za-z0-9]$/g.test(str);
+    return validUser
+  }
+  else return false
+}
+
+// keep this function call here 
+console.log(CodelandUsernameValidation('testerino__2')); //return true

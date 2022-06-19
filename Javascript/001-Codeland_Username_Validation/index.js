b3c6801b404d4ffd8bd7aafcbe8f85061c124778
Pedro Yanez <pedro_y91@outlook.com>
2022-06-18 20:13:19 -0400

    little fix

--- Javascript/001-Codeland_Username_Validation/index.js
+++ Javascript/001-Codeland_Username_Validation/index.js
@@ -1,6 +1,6 @@
 function CodelandUsernameValidation(str) {
   // code goes here
-  if (str.length > 3 && str.length < 25) {
+  if (str.length > 3 && str.length < 26) {
     const validUser = /^[A-Za-z]+[A-Za-z0-9_]+[A-Za-z0-9]$/g.test(str);
     return validUser
   }

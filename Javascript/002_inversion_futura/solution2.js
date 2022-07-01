726485d5ab622b9415bfaecda0680f12921999dd
Pedro Yanez <pedro_y91@outlook.com>
2022-07-01 07:26:04 -0400

    solution2.js

---
+++ Javascript/002_inversion_futura/solution2.js
@@ -0,0 +1,21 @@
+// SOLUCIÓN #2
+const acciones = [32, 35, 40, 21, 19, 27, 30];
+
+const f = (acc) => {
+  let MinValue = 32;
+  let ventaFinal = 0;
+
+  for (let day = 0; day < acc.length; day++) {
+
+    if (MinValue > acc[day]) {
+      MinValue = acc[day];
+    }
+
+    if (MinValue < acc[day]) {
+      ventaFinal = acc[day] - MinValue;
+    }
+  }
+
+  return (ventaFinal);
+}
+console.log(f(acciones)); //EXPECTED => 11

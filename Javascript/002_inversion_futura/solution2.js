2a99441e5463272c28debd6f0128f8393fb39e41
Pedro Yanez <pedro_y91@outlook.com>
2022-07-02 23:03:38 -0400

    better Minvalue value

--- Javascript/002_inversion_futura/solution2.js
+++ Javascript/002_inversion_futura/solution2.js
@@ -2,7 +2,7 @@
 const acciones = [32, 35, 40, 21, 19, 27, 30];
 
 const f = (acc) => {
-  let MinValue = 32;
+  let MinValue = acc[0];
   let ventaFinal = 0;
 
   for (let day = 0; day < acc.length; day++) {

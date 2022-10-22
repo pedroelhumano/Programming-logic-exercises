89d04f08e948dba0af83a00676f3cae8b5e7ad99
Pedro Yanez <pedro_y91@outlook.com>
2022-10-22 18:40:57 -0300

    add Java 001

---
+++ Java/001/PreguntaM1/README.md
@@ -0,0 +1,12 @@
+## 001
+
+Introducción a la programación orientada a objetos
+
+- Para realizar el cálculo de la nota de presentación a un examen, cada nota parcial se multiplica por un factor específico.
+- El resultado de la multiplicación se va acumulando hasta conformar un total que corresponde a la nota de presentación al examen.
+- Si la sumatoria acumulada es superior a un determinado valor, entonces el alumno tiene derecho a dar el examen. Si no, la sumatoria acumulada corresponde a la nota final.
+- En el siguiente cuadro de ejemplo:
+
+![](./img/cuadro.png)
+
+Dado el caso expuesto y considerando un desarrollo en JAVA, ¿Qué tipos de datos y que  operador aritmético-lógico debería utilizar dentro del programa para procesar las notas, obtener el promedio y verificar si el valor permite que el alumno pueda dar examen?
---
+++ Java/001/PreguntaM1/src/App.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Variables.
+        float nota[] = new float[6];
+        float factor[] = { 0.07f, 0.14f, 0.21f, 0.07f, 0.07f, 0.14f };
+        float notasFinal[] = new float[6];
+        float total = 0.0f;
+        Scanner SC = new Scanner(System.in);
+
+        // INICIO
+        System.out.println("Bienvenido a calculo de notas");
+
+        // Ingreso de notas.
+        for (int i = 0; i < notasFinal.length; i++) {
+            // Se guarda la nota
+            System.out.println("Ingrese la nota numero: " + (i + 1));
+
+            nota[i] = SC.nextFloat();
+            notasFinal[i] = nota[i] * factor[i];
+            // El total que se va a mostrar al final
+            total = total + notasFinal[i];
+        }
+
+        // FINAL
+        System.out.println("Tu nota es final es " + total);
+        // Caso aprobado solo mayor o igual a 4.
+        if (total >= 4) {
+            System.out.println("Usted puede presentar el examen final");
+        }
+        // Caso reprobado
+        else {
+            System.out.println("Reprobaste");
+        }
+        SC.close();
+    }
+}

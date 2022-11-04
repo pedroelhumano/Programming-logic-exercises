d613ca497c7c31b03f19dc28e632c555332eb836
Pedro Yanez <pedro_y91@outlook.com>
2022-11-04 00:58:50 -0300

    wip 003 Java

--- Java/003/src/App.java
+++ Java/003/src/App.java
@@ -1,5 +1,45 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Bienvenida a la tabla de multiplicar
+        System.out.println("Bienvenido a la tablaMultiplicator.v3.0.0");
+
+        // Variables.
+        Scanner SC = new Scanner(System.in);
+        String isProgramRunning = "si";
+        boolean catchingErrores = true;
+        int numero;
+        int limitTabla;
+
+        // Ciclo infinito para repetir la operacion hasta que se cancele
+        while (isProgramRunning == "si") {
+            catchingErrores = true;
+            System.out.print("Ingresa el numero que se desea conocer la tabla: ");
+            numero = SC.nextInt();
+            System.out.print("Ingresa hasta donde se desea conocer la tabla de multiplicar: ");
+            limitTabla = SC.nextInt();
+
+            Tabla tablamultiplicar = new Tabla(numero, limitTabla);
+            tablamultiplicar.TablaCalculator();
+
+            // Desea repetir?
+            while (catchingErrores) {
+                System.out.println("¿Desea Repetir?");
+                System.out.println("- si");
+                System.out.println("- no");
+                isProgramRunning = SC.nextLine();
+
+                if (isProgramRunning == "si")
+                    catchingErrores = false;
+
+                if (isProgramRunning == "no")
+                    catchingErrores = false;
+
+                if (isProgramRunning != "si" || isProgramRunning != "no")
+                    System.out.println("Ingrese un valor correcto: si o no");
+            }
+        }
+        SC.close();
     }
 }
---
+++ Java/003/src/Tabla.java
@@ -0,0 +1,19 @@
+public class Tabla {
+
+  private int numero;
+  private int limiteTabla;
+
+  public Tabla(int n1, int n2) {
+    numero = n1;
+    limiteTabla = n2;
+  }
+
+  public String TablaCalculator() {
+    for (int i = 0; i <= limiteTabla; i++) {
+      System.out.println(numero + " * " + i + " = " + (numero * i));
+    }
+    return null;
+
+  }
+
+}

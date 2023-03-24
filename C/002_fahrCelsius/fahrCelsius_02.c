89d6d87db38b15ffe942fdb05e01a6bb6452146f
Pedro Yanez <pedro_y91@outlook.com>
2023-03-24 12:26:09 -0300

    add constants

--- C/002_fahrCelsius/fahrCelsius_02.c
+++ C/002_fahrCelsius/fahrCelsius_02.c
@@ -5,6 +5,10 @@
   para fahr = 0, 20, ..., 300
 */
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 int main()
 {
   int fahr;
@@ -12,6 +16,6 @@ int main()
   printf("Program farh 02\n");
   printf("Fahrenheit\tCelsius\n");
 
-  for (fahr = 0; fahr <= 300; fahr = fahr + 20)
-    printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+  for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
+    printf("%3d %17.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
 }

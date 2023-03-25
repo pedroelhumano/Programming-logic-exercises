6ccc8f69053ebaa21091f2cdab55d8b4d7924662
Pedro Yanez <pedro_y91@outlook.com>
2023-03-25 11:35:59 -0300

    add notes

--- C/003_copyFiles/copy_files.c
+++ C/003_copyFiles/copy_files.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+/* capturar la entrada y mostrarla en pantalla*/
+
+/*
+  Nota adicional
+  Para generar artificialmente un EOF en la terminal se utiliza:
+  ctrl D en unix/linux/macos
+  ctrl Z en windows
+*/
+
 int main()
 {
   int c;

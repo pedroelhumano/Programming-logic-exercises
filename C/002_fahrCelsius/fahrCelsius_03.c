a9050e92697271860aaa5eb0a4b60ee5bcf74d49
Pedro Yanez <pedro_y91@outlook.com>
2023-04-04 10:46:16 -0400

    WIP fahrCelsius en progreso

---
+++ C/002_fahrCelsius/fahrCelsius_03.c
@@ -0,0 +1,24 @@
+// #include <stdio.h>
+
+/*
+  Programa para imprimir la tabla de fahrenheit-Celsius
+  Usando funciones
+  para fahr = 0, 20, ..., 300
+*/
+
+// Codear aqui
+
+// #define LOWER 0
+// #define UPPER 300
+// #define STEP 20
+
+// int main()
+// {
+//   int fahr;
+
+//   printf("Program farh 02\n");
+//   printf("Fahrenheit\tCelsius\n");
+
+//   for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
+//     printf("%3d %17.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+// }

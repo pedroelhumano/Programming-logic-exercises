a3b3f731baf8011191b3acc3e525b94d72c62407
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2023-04-08 20:27:07 -0400

    004_Median_of_Two_Sorted_Arrays

---
+++ Javascript/004_Median_of_Two_Sorted_Arrays/index.js
@@ -0,0 +1,21 @@
+// NOTE: If the union of both arrays is odd, then the median would be the
+// element in the middle of the array. If it is even, then the median would be
+// defined as the average of the two elements in the middle of the array.
+
+// CHANGE Arrs
+
+const arr1 = [5, 4, 3, 2, 1];
+// const arr2 = [10, 9, 8, 7, 6];
+const arr3 = [11, 10, 9, 8, 7, 6];
+
+const arrayCombination = arr1.concat(arr3).sort((a, b) => a - b);
+
+// arr odd
+if (arrayCombination.length % 2 !== 0) {
+  console.log('Mediana: ', arrayCombination[(arrayCombination.length - 1) / 2])
+
+}
+// arr even
+else {
+  console.log('Mediana: ', (arrayCombination[(arrayCombination.length - 2) / 2] + arrayCombination[((arrayCombination.length - 2) / 2) + 1]) / 2)
+}

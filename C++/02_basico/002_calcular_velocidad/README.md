32f88853404a040e75c015716e6f2fc640a19c65
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2023-04-28 21:46:51 -0400

    velocidad

---
+++ C++/02_basico/002_calcular_velocidad/README.md
@@ -0,0 +1,6 @@
+## Area de un circulo
+
+Hacer un programa que que calcule la velocidad de un vehiculo en base a los datos ingresados por el usuario. Los datos que puede ingresar el usuario son:
+
+- Punto de partida y punto de destino. Estos representan la distancia entre dos puntos por ejemplo inicia en 200mts y finaliza en 2800mts
+- Hora, minutos, y segundos transcurridos del recorrido
---
+++ C++/02_basico/002_calcular_velocidad/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+  int pInicial, pFinal, horas, minutos, segundos, distancia;
+
+  float velocidad, tiempoTranscurrido;
+
+  cout << "Ingresa el punto inicial de partida en metros " << flush;
+  cin >> pInicial;
+
+  cout << "Ingresa el punto final del recorrido en metros " << flush;
+  cin >> pFinal;
+
+  distancia = pFinal - pInicial;
+
+  cout << "Ingresa la cantidad de horas que tardo el recorrido " << flush;
+  cin >> horas;
+
+  cout << "Ingresa la cantidad de minutos que tardo el recorrido " << flush;
+  cin >> minutos;
+
+  cout << "Ingresa la cantidad de segundos que tardo el recorrido " << flush;
+  cin >> segundos;
+
+  tiempoTranscurrido = horas + (minutos / 60) + (segundos / 3600);
+  velocidad = distancia / tiempoTranscurrido;
+
+  cout << "La velocidad del vehiculo es de: " << velocidad << " metros por hora" << flush;
+
+  return 0;
+}

096dac37b075ab4db50f13e3ee7c4c1e2f31ee61
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2023-04-28 21:34:33 -0400

    area de un circulo

---
+++ C++/01_iniciacion/003_area_circulo/README.md
@@ -0,0 +1,3 @@
+## Area
+
+Hacer un programa que calcule el area de un circulo. Formula: area = 3.1415 * radio * radio
---
+++ C++/01_iniciacion/003_area_circulo/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+  int radio;
+  float area;
+
+  cout << "Ingresa el radio: " << flush;
+  cin >> radio;
+
+  area = 3.1415 * radio * radio;
+
+  cout << "El area es: " << area << endl;
+
+  return 0;
+}
